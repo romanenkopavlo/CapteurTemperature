728c5b9cc04281f1a814a9c9976a1ca89e690ecc
utopiaforever <pavlo.romanenko@lyceeastier.com>
2024-01-27 01:37:09 +0100

    La version definitive

--- src/main/java/org/example/monitoringtemperatures/FXML_Controller.java
+++ src/main/java/org/example/monitoringtemperatures/FXML_Controller.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Cursor;
 import javafx.scene.Node;
+import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
@@ -17,13 +18,11 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
-import java.util.ResourceBundle;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.*;
 
 public class FXML_Controller implements Initializable {
     @FXML
-    NumberAxis xAxis;
+    CategoryAxis xAxis;
     @FXML
     NumberAxis yAxis;
     @FXML
@@ -31,21 +30,23 @@ public class FXML_Controller implements Initializable {
     @FXML
     CheckBox checkBox_Continu;
     @FXML
-    LineChart<Number, Number> lineChart;
+    LineChart<String, Number> lineChart;
     @FXML
     Label label_Valeur;
     @FXML
     Label cursorCoords;
-    XYChart.Series<Number, Number> series = new XYChart.Series<>();
+    XYChart.Series<String, Number> series = new XYChart.Series<>();
     String port = "COM10";
     boolean stop = false;
-    SimpleDateFormat simpleDateFormat;
+    final int TAILLE_DE_FENETRE = 15;
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
     DecimalFormat df = new DecimalFormat("0.##");
     Timer timer;
     CapteurTemperature capteurTemperature;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        lineChart.setAnimated(false);
         lineChart.setTitle("Graphique");
         lineChart.setCursor(Cursor.CROSSHAIR);
         series.setName("Capteur Z56");
@@ -68,13 +69,12 @@ public class FXML_Controller implements Initializable {
 
         chartBackground.setOnMouseEntered(event -> cursorCoords.setVisible(true));
         chartBackground.setOnMouseExited(event -> cursorCoords.setVisible(false));
-        chartBackground.setOnMouseMoved(event ->  cursorCoords.setText("Time: " + df.format(xAxis.getValueForDisplay(event.getX())) + "  °C: " + df.format(yAxis.getValueForDisplay(event.getY()))));
+        chartBackground.setOnMouseMoved(event ->  cursorCoords.setText("Time: " + xAxis.getValueForDisplay(event.getX()) + "  °C: " + df.format(yAxis.getValueForDisplay(event.getY()))));
 
         checkBox_Continu.setOnAction(event -> stop = checkBox_Continu.isSelected());
     }
     private void base_De_Temps() {
         TimerTask updateTask = new TimerTask() {
-            int iteration = 0;
             @Override
             public void run() {
                 if (!stop) {
@@ -84,10 +84,12 @@ public class FXML_Controller implements Initializable {
                             capteurTemperature.ecrire("IEEE".getBytes(StandardCharsets.US_ASCII));
                             capteurTemperature.ecrire("\r".getBytes(StandardCharsets.US_ASCII));
                             Thread.sleep(1000);
-                            series.getData().add(new XYChart.Data<>(iteration, capteurTemperature.getValeurLue()));
+                            series.getData().add(new XYChart.Data<>(simpleDateFormat.format(new Date()), capteurTemperature.getValeurLue()));
                             label_Valeur.setText("Value received: " + df.format(capteurTemperature.getValeurLue()));
+                            if (series.getData().size() > TAILLE_DE_FENETRE) {
+                                series.getData().remove(0);
+                            }
                             capteurTemperature.fermerLiaisonSerieCapteur();
-                            iteration += 5;
                         } catch (SerialPortException | InterruptedException e) {
                             throw new RuntimeException(e);
                         }
